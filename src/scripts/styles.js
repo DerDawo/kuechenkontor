cb5db0005ac2fbd8342e067f72585a1309440577
DerDawo <71173937+DerDawo@users.noreply.github.com>
2024-02-25 11:10:30 +0000

    material nav and display

--- src/scripts/styles.js
+++ src/scripts/styles.js
@@ -26,3 +26,23 @@ function unhoverNav(){
     if(scrollTop > 100) return
     nav.classList.remove("hover")
 }
+
+const materialNavOptions = document.getElementsByClassName("material-nav-container")[0].querySelectorAll("input");
+console.log(materialNavOptions)
+for(const mnO of materialNavOptions){
+    mnO.addEventListener("click",showMaterialContainerDiv)
+}
+
+function showMaterialContainerDiv(){
+    const materialContainerDivs = document.getElementsByClassName("material-display-container")[0].getElementsByTagName("div")
+    const activeMaterialContainerDiv = document.getElementsByClassName("material-display-container")[0].querySelector(`[data-for="${this.id}"]`)
+
+    for(const mcD of materialContainerDivs){
+        if(mcD == activeMaterialContainerDiv){
+            mcD.classList.add("show-data")
+        } else {
+            mcD.classList.remove("show-data")
+        }
+    }
+    
+}
