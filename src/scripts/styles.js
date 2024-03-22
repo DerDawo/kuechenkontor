2f2c825bfe37950e8b9673b87e2df5a9057f5795
DerDawo <71173937+DerDawo@users.noreply.github.com>
2024-03-22 11:12:07 +0000

    fade-in fix

--- src/scripts/styles.js
+++ src/scripts/styles.js
@@ -1,35 +1,4 @@
-/*
-const nav = document.querySelector("nav")
-
-let scrollTop = 0
-
-function changeNav() {
-    fadeInOnScroll()
-    scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    
-    console.log(scrollTop)
-    
-    if (scrollTop > 100) {
-        hoverNav()
-    } else {
-        unhoverNav()
-    }
-}
-
-window.addEventListener("scroll", changeNav, false);
-//document.addEventListener("scroll",fadeInOnScroll)
-
-nav.addEventListener("mouseover",hoverNav)
-nav.addEventListener("mouseleave",unhoverNav)
-
-function hoverNav(){
-    nav.classList.add("hover")
-}
-function unhoverNav(){
-    if(scrollTop > 100) return
-    nav.classList.remove("hover")
-}
-*/
+window.addEventListener("scroll", fadeInOnScroll, false);
 
 const materialNavOptions = document.getElementsByClassName("material-nav-container")[0].querySelectorAll("input");
 for(const mnO of materialNavOptions){
